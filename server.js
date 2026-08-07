4c1753487a9ff546dc79daa13a27111def8a0601
Haytham G. Zeidan <83829287+wazefapress@users.noreply.github.com>
2026-08-07 09:21:47 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -32,4 +32,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => console.log('الخادم يعمل على الرابط: http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`الخادم يعمل على المنفذ: ${PORT}`);
+});
